b0b753d8e4b62e0237cf7404e02841e88d46a6c9
touunyy <tony.andriamampianina@epfl.ch>
2024-02-21 20:08:21 +0100

    commit animal pos zone

--- ChaCuN/src/ch/epfl/chacun/Zone.java
+++ ChaCuN/src/ch/epfl/chacun/Zone.java
@@ -1,6 +1,10 @@
 package ch.epfl.chacun;
 
+import org.w3c.dom.ls.LSException;
+
 import java.util.List;
+import java.util.Objects;
+import java.util.TreeMap;
 
 public interface Zone {
     //les pouvoirs possibles des zones,
@@ -13,7 +17,7 @@ public interface Zone {
     static int localId(int zoneId) {
         return zoneId % 10;
     }
-    public abstract int id (int zoneId);
+    public abstract int id ();
     default int tileId(){return 0;}
     default int localId(){return 0;}
     default SpecialPower specialPower() {return null; }
@@ -25,11 +29,24 @@ public interface Zone {
              PLAIN, WITH_MENHIR, WITH_MUSHROOMS;
         }
     }
-    public record Meadow implements Zone(int id, List <Animal> animals, ) {
-
-
+    public record Meadow (int id, List <Animal> animals, SpecialPower specialPower) implements Zone{
+        public int id() {return id;}
+        public Meadow {
+            animals = List.copyOf(animals);
+        }
     }
+    public interface Water extends Zone {
+        public abstract int fishCount ();
     }
-
+    public record Lake (int id, int fishCount, SpecialPower specialPower) implements Water {
+        @Override
+        public int id() {return id;}
+    }
+    public record River (int id, int fishCount, Lake lake) implements Water{
+        boolean hasLake() {
+            return lake != null;
+    }
+    public int fishCount() {return fishCount;}
+}
 }
 
