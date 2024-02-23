2b39ba130c76bb836b3ce5e6b9be648ca2a66537
hassenBC <hassen.benchaabane@epfl.ch>
2024-02-23 10:19:21 +0100

    LE CODE

--- ChaCuN/src/ch/epfl/chacun/TileSide.java
+++ ChaCuN/src/ch/epfl/chacun/TileSide.java
@@ -1,6 +1,19 @@
 package ch.epfl.chacun;
 
+import java.util.List;
+
 public interface TileSide {
+    public abstract List<Zone> zones();
+    public abstract boolean isSameKindAs(TileSide that);
+}
+ record Forest(Zone.Forest forest) implements TileSide{
+    @Override
+    public List<Zone> zones() {
+        return null;
+    }
 
-
+    @Override
+    public boolean isSameKindAs(TileSide that) {
+        return that instanceof Zone.Forest;
+    }
 }
