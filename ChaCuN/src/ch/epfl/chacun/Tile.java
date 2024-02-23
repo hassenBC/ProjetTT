847e2799de9da5d76bc40a9adc3a53be223af298
touunyy <tony.andriamampianina@epfl.ch>
2024-02-23 15:00:24 +0100

    tile finished and tiledecks not yet

--- ChaCuN/src/ch/epfl/chacun/Tile.java
+++ ChaCuN/src/ch/epfl/chacun/Tile.java
@@ -3,8 +3,18 @@ import ch.epfl.chacun.TileSide;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
+/** Tuile de jeu 
+ * @author Tony Andriamampianina (SCIPER)
+ * @param id
+ * @param kind
+ * @param n
+ * @param e
+ * @param s
+ * @param w
+ */
 public record Tile(int id, Kind kind, TileSide n, TileSide e, TileSide s, TileSide w) {
     public enum Kind {
         START, NORMAL, MENHIR }
@@ -16,9 +26,34 @@ public record Tile(int id, Kind kind, TileSide n, TileSide e, TileSide s, TileSi
         sides.add(w);
         return sides;
     }
+
+    /**
+     * ensemble de toutes les zones en bordure de la tuile (qu'on obtient grâce aux attributs tileside)
+     * @return un ensemble de Zones sideZones
+     */
     public Set<Zone> sideZones() {
-        return null;
+        Set <Zone> sideZones = new HashSet<>();
+        sideZones.addAll(n.zones());
+        sideZones.addAll(e.zones());
+        sideZones.addAll(s.zones());
+        sideZones.addAll(w.zones());
+        return sideZones;
     }
+    
+    public Set<Zone> zones() {
+        Set <Zone> zones = new HashSet<>();
+        zones.addAll(sideZones());
+        for (Zone zone : zones) {
+            if (zone instanceof Zone.River river) {
+                /** en faisant instanceof on crée une instance de river dont on utilise les méthodes */
+                if ((river.hasLake())) {
+                    zones.add(river.lake());
+                }
+            }
+        } return zones;
+    }
+    
+    
 
 
 }
---
+++ ChaCuN/src/ch/epfl/chacun/TileDecks.java
@@ -0,0 +1,19 @@
+package ch.epfl.chacun;
+
+import java.util.List;
+
+public final record TileDecks(List<Tile> startTiles, List <Tile> normalTiles, List <Tile> menhirTiles) {
+    public TileDecks {
+        startTiles = List.copyOf(startTiles);
+    }
+    public int deckSize(Tile.Kind kind) {
+        switch (kind)
+
+        List <Tile>
+        for (:
+             ) {
+            
+        }
+    }
+    
+}
