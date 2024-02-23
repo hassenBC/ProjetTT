199a688f1667398b793d86e42264b753b13e9e6c
touunyy <tony.andriamampianina@epfl.ch>
2024-02-23 16:33:23 +0100

    tiledecks -last method

--- ChaCuN/src/ch/epfl/chacun/TileDecks.java
+++ ChaCuN/src/ch/epfl/chacun/TileDecks.java
@@ -1,19 +1,73 @@
 package ch.epfl.chacun;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+/** enregistrement des piles de tuiles
+ * @author Tony Andriamampianina (363559)
+ * @param startTiles
+ * @param normalTiles
+ * @param menhirTiles
+ */
 public final record TileDecks(List<Tile> startTiles, List <Tile> normalTiles, List <Tile> menhirTiles) {
+    /** constructeur chargé de faire une copie défensive de chacune des listes
+     * @param startTiles
+     * @param normalTiles
+     * @param menhirTiles
+     */
     public TileDecks {
         startTiles = List.copyOf(startTiles);
+        normalTiles = List.copyOf(normalTiles);
+        menhirTiles = List.copyOf(menhirTiles);
     }
-    public int deckSize(Tile.Kind kind) {
-        switch (kind)
 
-        List <Tile>
-        for (:
-             ) {
-            
+    /** méthode utile pour return le deck du kind donné en paramètre
+     * @param kind
+     * @return List Deck
+     */
+    private List <Tile> fromKindtoDeck (Tile.Kind kind) {
+        List<Tile> deck = new ArrayList<Tile>();
+        switch (kind) {
+            case START -> deck = List.copyOf(startTiles);
+            case NORMAL -> deck = List.copyOf(normalTiles);
+            case MENHIR -> deck = List.copyOf(menhirTiles);
+        }
+        return deck;
+    }
+
+
+    /** à partir du type de tile en paramètre, retourne la taille du deck
+     * @param kind
+     * @return
+     */
+    public int deckSize(Tile.Kind kind) {
+        List<Tile> deck = fromKindtoDeck(kind);
+        return deck.size();
+    }
+    public Tile topTile (Tile.Kind kind) {
+        if (deckSize(kind) != 0) {
+            List<Tile> deck = fromKindtoDeck(kind);
+            return deck.get(0);
+        } else {
+            return null;}
+    }
+    public TileDecks withTopTileDrawn (Tile.Kind kind) {
+        if (fromKindtoDeck(kind).isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            List <Tile> deck = fromKindtoDeck(kind);
+            deck.removeFirst();
+            return switch (kind) {
+                case START -> (new TileDecks(deck, normalTiles, menhirTiles));
+                case NORMAL -> (new TileDecks(startTiles, deck, menhirTiles));
+                case MENHIR -> (new TileDecks(startTiles, normalTiles, deck));
+            };
         }
     }
+    public TileDecks withTopTileDrawnUntil (Tile.Kind kind, Predicate <Tile> predicate) {
+
+    }
+
     
 }
