c8f7a4a1b714ff92aaa35b94ab937fa79a24e790
touunyy <tony.andriamampianina@epfl.ch>
2024-02-28 19:34:48 +0100

    test tile

--- ChaCuN/test/ch/epfl/chacun/TileTest.java
+++ ChaCuN/test/ch/epfl/chacun/TileTest.java
@@ -2,14 +2,24 @@ package ch.epfl.chacun;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TileTest {
-    TileSide.Meadow meadow1 = new TileSide.Meadow(new Zone.Meadow(560, null, null));
-    TileSide.Forest forest1 = new TileSide.Forest(new Zone.Forest(561, Zone.Forest.Kind.PLAIN));
-    TileSide.River river1 = new TileSide.River(new Zone.Meadow(562, null, null), new Zone.River(563, 0, new Zone.Lake(568, 0, null)), (new Zone.Meadow(560, null, null)));
+    Zone.Meadow meadowZone1 = new Zone.Meadow(560, null, null);
+    Zone.Meadow meadowZone2 = new Zone.Meadow(562, null, null);
+    Zone.Forest forestZone = new Zone.Forest(561, Zone.Forest.Kind.PLAIN);
+    Zone.Lake lakeZone = new Zone.Lake(568, 0, null)
+    Zone.River riverZone = new Zone.River(563, 0, lakeZone);
+
+    TileSide.Meadow meadow1 = new TileSide.Meadow(meadowZone1);
+    TileSide.Meadow meadow2 = new TileSide.Meadow(meadowZone2);
+    TileSide.Forest forest1 = new TileSide.Forest(forestZone);
+    TileSide.River river1 = new TileSide.River(meadowZone2, riverZone, meadowZone1);
+
     Tile start = new Tile(56, Tile.Kind.START, meadow1, forest1, forest1, river1);
 
     @Test
@@ -18,9 +28,15 @@ public class TileTest {
         sides.add(meadow1);
         sides.add(forest1);
         sides.add(river1);
-        assertEquals(start.sides(), sides);
+        assertEquals(sides, start.sides());
     }
-    void sideZones() {
-
+    @Test
+    void sideZonesTest() {
+        Set <Zone> sideZones = new HashSet<>();
+        sideZones.add(meadowZone1);
+        sideZones.add(forestZone);
+        sideZones.add(meadowZone2);
+        sideZones.add(riverZone);
+        assertEquals(sideZones ,start.sideZones() );
     }
 }
