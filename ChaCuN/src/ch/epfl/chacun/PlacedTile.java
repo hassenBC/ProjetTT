273c47eb19dcb114a0424bd20d5547441c65d73a
hassenBC <hassen.benchaabane@epfl.ch>
2024-03-01 13:48:04 +0100

    Les test sont presque fini

--- ChaCuN/src/ch/epfl/chacun/PlacedTile.java
+++ ChaCuN/src/ch/epfl/chacun/PlacedTile.java
@@ -2,23 +2,65 @@ package ch.epfl.chacun;
 
 import java.util.*;
 
+/**
+ * @author Hassen Ben Chaabane (361366)
+ *Constructeur de PlacedTile
+ * @param tile
+ * @param placer
+ * @param rotation
+ * @param pos
+ * @param occupant
+ */
 public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos pos, Occupant occupant ) {
+
+    /**
+     * Construteur compact de PlacedTile pour check les préconditions
+     * @param tile
+     * @param placer
+     * @param rotation
+     * @param pos
+     * @param occupant
+     */
     public PlacedTile{
         Objects.requireNonNull(tile);
         Objects.requireNonNull(rotation);
         Objects.requireNonNull(pos);
     }
+
+    /**
+     * Constructeur de copie de PlacedTile qui donne une placedTile identique mais sans occupant
+     * @param tile
+     * @param placer
+     * @param rotation
+     * @param pos
+     */
+
      public PlacedTile(Tile tile,PlayerColor placer,Rotation rotation,Pos pos){
         this(tile,placer,rotation,pos,null);
     }
 
+    /**
+     * Getter de l'id de la tuile
+     * @return L'id de la tuile utilisée
+     */
+
     public int id(){
         return tile.id();
     }
+
+    /**
+     * Getter du Kind de la tuile par exemple dans la premiere tuile placée il return START
+     * @return Le kind de la tuile.
+     */
     public Tile.Kind kind(){
         return tile.kind();
     }
 
+    /**
+     * Méthode qui applique la rotation donnée par le constructeur sur la tuile
+     * @param direction
+     * @return les nouvelles directions des zones de nos tuiles
+     */
     public TileSide side(Direction direction){
 
          Direction newDirection = direction.rotated(rotation.negated());
@@ -27,13 +69,14 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
             case E -> tile.e();
             case S -> tile.s();
             case W -> tile.w();
-        };//A TESTER RIGOUREUSEMENT !!!!!!!!!!!!!!!!!!!
+        };
     }
 
     /**
-     *
+     *La méthode va itérer sur toutes les zones de la tuile et return celle qui valide l'id qu'on lui donne.
+     * @throws IllegalArgumentException si la tuile n'a pas un id valide
      * @param id
-     * @return
+     * @return La zone avec l'id rentré en paramètre
      */
     public Zone zoneWithId(int id){
         //il va itérer sur toutes les zones de la tuile return celle qui valide l'id qu'on lui donne.
@@ -43,6 +86,11 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
        }
        throw new IllegalArgumentException("Tuile sans id valide");
     }
+
+    /**
+     * Comme on sait qu'une tuile peut avoir qu'un seul special power on itère sur les zone et on rends le special power
+     * @return special power de la zone ou null si il n'y a pas de special power
+     */
 
     public Zone specialPowerZone(){
         Set<Zone> zones = this.tile.zones();
@@ -54,6 +102,11 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
         return null;
     }
 
+    /**
+     * Itère sur toutes les zones et rends les zones Forest
+     * @return Un Set avec toutes les forets différentes
+     */
+
     public Set<Zone.Forest> forestZones(){
         Set<Zone.Forest> zonesForest = new HashSet<>();
         Set<Zone> zones = this.tile.zones();
@@ -62,6 +115,11 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
         }
         return zonesForest;
     }
+
+    /**
+     * Itère sur les zones de Tile
+     * @return Les zones Meadow de la tuile
+     */
     public Set<Zone.Meadow> meadowZones(){
         Set<Zone.Meadow> zonesMeadow = new HashSet<>();
         Set<Zone> zones = this.tile.zones();//trouver un moyen d'optimiser ça
@@ -71,6 +129,10 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
         return zonesMeadow;
     }
 
+    /**
+     * Itère sur les zones de Tile
+     * @return Les zones Rivers de la tuile
+     */
     public Set<Zone.River> riverZones(){
         Set<Zone.River> zonesRiver = new HashSet<>();
         Set<Zone> zones = this.tile.zones();
@@ -80,44 +142,68 @@ public record PlacedTile(Tile tile,PlayerColor placer, Rotation rotation, Pos po
         return zonesRiver;
     }
 
+    /**
+     * Itère sur toutes les zones et check la condition Lake de River pour savoir placer un PAWN ou une HUT
+     * @return Un set vide si placer est null
+     * @return Un set avec tous les potential occupants de notre tuile
+     */
+
     public Set<Occupant> potentialOccupants(){
-        int i = 0;
-        int j =8;
+        //demander aux assistants la correction
         Set<Zone> zones = this.tile.zones();
         Set<Occupant> potentialOccupants = new HashSet<>();
         if (placer == null){ return potentialOccupants; }
 
             for(Zone zone : zones){
                 if (zone instanceof Zone.Meadow ) {
-                     potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,i));// un pion (chasseur) occupant la zone 0 (le pré contenant l'auroch)
-                    ++i;
+                     potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,zone.id()));// un pion (chasseur) occupant la zone 0 (le pré contenant l'auroch)
+
                 }
                 else if (zone instanceof Zone.Forest) {
-                    potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,i));// un pion (cueilleur) occupant la zone 1 (la forêt)
-                    ++i;
+                    potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,zone.id()));// un pion (cueilleur) occupant la zone 1 (la forêt)
+
                 }
                 else if (zone instanceof Zone.River) {
-                    potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,i));// un pion (pêcheur) occupant la zone 3 (la rivière)
-                    ++i;
+                    potentialOccupants.add(new Occupant(Occupant.Kind.PAWN,zone.id()));// un pion (pêcheur) occupant la zone 3 (la rivière)
+                    if(!((Zone.River) zone).hasLake()){
+                    potentialOccupants.add(new Occupant(Occupant.Kind.HUT,zone.id()));
+                    }
                 }
                 else if (zone instanceof Zone.Lake ) {
-                    potentialOccupants.add(new Occupant(Occupant.Kind.HUT, j));// une Hutte (pêcheur) occupant le lac zone 8
-                    ++j;
+                    potentialOccupants.add(new Occupant(Occupant.Kind.HUT, zone.id()));// une Hutte (pêcheur) occupant le lac zone 8
+
                 }
 
                 //Ajouter une condition comme quoi le nombre d'occupant ne peut pas dépasser le nombre de zones dans la tuile
         }
          return potentialOccupants;
     }
+
+    /**
+     * @throws IllegalArgumentException si la tuile ne possède pas un occupant
+     * @param occupant
+     * @return Une placed TIle identique à celle du constructeur mais avec un paramètre en plus l'occupant
+     */
     public PlacedTile withOccupant(Occupant occupant){
         Preconditions.checkArgument(this.occupant==null);
         return new PlacedTile(this.tile, this.placer, this.rotation, this.pos, occupant);
         //on place une tuile avec l'occupant qu'on veut.
     }
 
+    /**
+     * Donne une nouvelle PlacedTile sans l'occupant.
+     * @return Nouvelle placedTile sans occupant
+     */
     public PlacedTile withNoOccupant(){
         return new PlacedTile(this.tile, this.placer, this.rotation, this.pos);
     }
+
+    /**
+     *
+     * @param occupantKind
+     * @return L'id de la zone de l'occupant mis en paramètre par le constructeur de placedtile
+     * ou donne -1 si occupant == null
+     */
 
 
     public int idOfZoneOccupiedBy(Occupant.Kind occupantKind){
--- ChaCuN/src/ch/epfl/chacun/TileSide.java
+++ ChaCuN/src/ch/epfl/chacun/TileSide.java
@@ -3,47 +3,89 @@ package ch.epfl.chacun;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Hassen Ben Chaabane (361366)
+ */
 public interface TileSide {
     List<Zone> zones();
-    boolean isSameKindAs(TileSide that);
-    //initialiser nos élèments ???
 
- record Forest(Zone.Forest forest) implements TileSide{
+    /**
+     *
+     * @param that La zone qu'on veut comparer
+     * @return False si les zones ne sont pas du meme type, True si oui.
+     */
+    boolean isSameKindAs(TileSide that);
+
+    /**
+     * Record de Forest
+     * @param forest La foret qui touche le bord de la tuile
+     */
+    record Forest(Zone.Forest forest) implements TileSide{
     @Override
     public List<Zone> zones() {
         //verifie avec une zone autre que forest ptt rajouter un test
         return List.of(forest);
     }
-
+     /**
+      *
+      * @param that La zone qu'on veut comparer
+      * @return False si les zones ne sont pas du meme type, True si oui.
+      */
     @Override
     public boolean isSameKindAs(TileSide that) {
         return that instanceof TileSide.Forest;
         }
     }
 
+    /**
+     *
+     * @param meadow Le meadow qui touche le bord de la tuile
+     */
     record Meadow(Zone.Meadow meadow) implements TileSide{
+        /**
+         * On sait que Meadow peut être en contact qu'avec les autres meadow
+         * @return Les zones qui peuvent etre en contact avec meadwow
+         */
 
         @Override
         public List<Zone> zones() {
             //vérifie avec une zone autre que meadow
             return List.of(meadow);
         }
-
+        /**
+         *
+         * @param that La zone qu'on veut comparer
+         * @return False si les zones ne sont pas du meme type, True si oui.
+         */
         @Override
         public boolean isSameKindAs(TileSide that) {
-            //ou this == that non puisqu'il va comparer directement les références
             return that instanceof TileSide.Meadow;
         }
     }
 
+    /**
+     * Record qui prend en paramètre la river et les deux meadow qui l'entoure
+     * @param meadow1
+     * @param river
+     * @param meadow2
+     */
+
     record River(Zone.Meadow meadow1, Zone.River river, Zone.Meadow meadow2) implements TileSide{
 
+        /**
+         * On sait qu'une River doit etre en contact avec deux meadow
+         * @return La river qui peut entre en contact avec elle et les deux meadows qui l'entourent(dans le sens d'aiguille d'une montre)
+         */
         @Override
         public List<Zone> zones() {
             //ne pas tout mettre
             return List.of(meadow1,river,meadow2);
         }
-
+        /**
+         *
+         * @param that La zone qu'on veut comparer
+         * @return False si les zones ne sont pas du meme type, True si oui.
+         */
         @Override
         public boolean isSameKindAs(TileSide that) {
             return that instanceof TileSide.River;
