13a44b6a1e45e26a805cced98a0002e4e319d4ef
touunyy <tony.andriamampianina@epfl.ch>
2024-03-04 01:53:25 +0100

    zone partition presque finie

--- ChaCuN/src/ch/epfl/chacun/ZonePartition.java
+++ ChaCuN/src/ch/epfl/chacun/ZonePartition.java
@@ -26,6 +26,7 @@ public record ZonePartition <Z extends Zone> (Set <Area<Z>> areas) {
             this.areas = partition.areas();
 
         }
+        //demander si j'ai le droit de rendre areacontaining static
         public void addSingleton (Z zone, int openConnections) {
             Set <Z> singleZone = new HashSet<>();
             singleZone.add(zone);
@@ -53,11 +54,60 @@ public record ZonePartition <Z extends Zone> (Set <Area<Z>> areas) {
             else {
                     throw new IllegalArgumentException("Zone does not belong to any area or area is already occupied.");
                 }
-            }
-
         }
 
+        public void removeOccupant (Z zone, PlayerColor color) {
+            boolean updated = false;
+            Area <Z> areaToUpdate = null;
+            for (Area <Z> area : areas) {
+                if (area.zones().contains(zone) && area.occupants().contains(color)) {
+                    areaToUpdate = area;
+                    updated = true;
+                    break;
+                }
+            }
+            if (updated) {
+                List<PlayerColor> oldOccupant = new ArrayList<>(areaToUpdate.occupants());
+                oldOccupant.remove(color);
+                Area<Z> newArea = new Area<>(areaToUpdate.zones(), oldOccupant, areaToUpdate.openConnections());
+                areas.remove(areaToUpdate);
+                areas.add(newArea);
+            }
+            else {
+                throw new IllegalArgumentException("Zone does not belong to any area or area is not occupied.");
+            }
+        }
 
+        public void removeAllOccupantsOf (Area <Z> area) {
+            if (areas.contains(area)) {
+                List <PlayerColor> noOccupant = new ArrayList<>();
+                Area <Z> newArea = new Area <> (area.zones(), noOccupant, area.openConnections());
+                areas.remove(area);
+                areas.add(newArea);
+            } else {
+                throw new IllegalArgumentException("Area does not belong in the partition");
+            }
+        }
+        public void union (Z zone1, Z zone2) {
+            boolean zone1Belongs = false;
+            boolean zone2Belongs = false;
+            boolean sameArea = false;
+            for (Area <Z> area : areas) {
+                if (area.zones().contains(zone1) && area.zones().contains(zone2)) {
+                    Area <Z> newArea = new Area<>(area.zones(), area.occupants(), area.openConnections()-2);
+                    areas.remove(area);
+                    areas.add(newArea);
+                    sameArea = true;
+                    break;
+                } else if (area.zones().contains(zone1) && !area.zones().contains(zone2)) {
+                    zone1Belongs = true;
+                    
+                } else if (!area.zones().contains(zone1) && area.zones().contains(zone2)) {
+                    
+                }
+            }
+        }
     }
+}
 
 
