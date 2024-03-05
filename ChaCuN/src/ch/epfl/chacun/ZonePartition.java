49d8b5c41f68515fa8e2af62e1c3b7570f082283
touunyy <tony.andriamampianina@epfl.ch>
2024-03-05 09:58:57 +0100

    tiledecks fixed

--- ChaCuN/src/ch/epfl/chacun/ZonePartition.java
+++ ChaCuN/src/ch/epfl/chacun/ZonePartition.java
@@ -92,6 +92,13 @@ public record ZonePartition <Z extends Zone> (Set <Area<Z>> areas) {
             boolean zone1Belongs = false;
             boolean zone2Belongs = false;
             boolean sameArea = false;
+            Set <Z> zones1 = new HashSet<>();
+            List <PlayerColor> colors1 = new ArrayList<>();
+            int oc1 = 0;
+            Set <Z> zones2 = new HashSet<>();
+            List <PlayerColor> colors2 = new ArrayList<>();
+            int oc2 = 0;
+
             for (Area <Z> area : areas) {
                 if (area.zones().contains(zone1) && area.zones().contains(zone2)) {
                     Area <Z> newArea = new Area<>(area.zones(), area.occupants(), area.openConnections()-2);
@@ -99,13 +106,28 @@ public record ZonePartition <Z extends Zone> (Set <Area<Z>> areas) {
                     areas.add(newArea);
                     sameArea = true;
                     break;
+
                 } else if (area.zones().contains(zone1) && !area.zones().contains(zone2)) {
                     zone1Belongs = true;
-                    
+                    zones1.addAll(area.zones());
+                    colors1.addAll(area.occupants());
+                    oc1 = area.openConnections();
+                    areas.remove(area);
+
                 } else if (!area.zones().contains(zone1) && area.zones().contains(zone2)) {
-                    
+                    zone2Belongs = true;
+                    zones2.addAll(area.zones());
+                    colors2.addAll(area.occupants());
+                    oc2 = area.openConnections();
+                    areas.remove(area);
                 }
-            }
+            } if (zone1Belongs && zone2Belongs && !sameArea) {
+                Area <Z> newArea1 = new Area<>(zones1, colors1, oc1);
+                Area <Z> newArea2 = new Area<>(zones2, colors2, oc2);
+                newArea1.connectTo(newArea2);
+
+
+            } else if (!zone1Belongs || !zone2Belongs || )
         }
     }
 }
