740f331063913f757fd549c19c30d05c633d2f6b
hassenBC <hassen.benchaabane@epfl.ch>
2024-03-07 20:24:22 +0100

    Area Finie !!!!! AAAAAAA

---
+++ ChaCuN/test/ch/epfl/chacun/HassenPartitionTests.java
@@ -0,0 +1,119 @@
+package ch.epfl.chacun;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ch.epfl.chacun.ZonePartition;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HassenPartitionTests {
+    HassenPartitionTests(){
+    }
+
+    @Test
+    void zonePartitionConstructorTrivialWorks(){
+
+        Set<Zone> zoneSet = new HashSet<>();
+        List<PlayerColor> playerColList = new ArrayList<>();
+        Area<Zone> testArea = new Area<>(zoneSet, playerColList,2);
+
+
+    }
+
+    @Test
+    void areaContainingTestWorksBasic(){
+        Set<Zone.Forest> zoneSet1 = new HashSet<>();
+        Set<Zone.Forest> zoneSet2 = new HashSet<>();
+        Set<Area<Zone.Forest>> areaSet = new HashSet<>();
+        List<PlayerColor> playerColorList = new ArrayList<>();
+        Zone.Forest zoneN = new Zone.Forest(10, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneE = new Zone.Forest(11, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneS = new Zone.Forest(12, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneNN = new Zone.Forest(13, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneEE = new Zone.Forest(14, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneSS = new Zone.Forest(15, Zone.Forest.Kind.PLAIN);
+        zoneSet1.add(zoneN);zoneSet1.add(zoneE);zoneSet1.add(zoneS);
+        zoneSet2.add(zoneNN); zoneSet2.add(zoneSS); zoneSet2.add(zoneEE);
+        Area<Zone.Forest> forestAreaTest2 = new Area<>(zoneSet2,playerColorList,2);
+        Area<Zone.Forest> forestAreaTest = new Area<>(zoneSet1,playerColorList,2);
+        areaSet.add(forestAreaTest2); areaSet.add(forestAreaTest);
+        ZonePartition <Zone.Forest> test = new ZonePartition<>(areaSet);
+        assertEquals(forestAreaTest,test.areaContaining(zoneE));
+    }
+
+    @Test
+    void areaContainingTestWorksLimits(){
+        Set<Zone.Forest> zoneSet1 = new HashSet<>();
+        Set<Zone.Forest> zoneSet2 = new HashSet<>();
+        Set<Area<Zone.Forest>> areaSet = new HashSet<>();
+        List<PlayerColor> playerColorList = new ArrayList<>();
+        Zone.Forest zoneN = new Zone.Forest(10, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneE = new Zone.Forest(11, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneS = new Zone.Forest(12, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneNN = new Zone.Forest(13, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneEE = new Zone.Forest(14, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneSS = new Zone.Forest(15, Zone.Forest.Kind.PLAIN);
+        zoneSet1.add(zoneN);zoneSet1.add(zoneS);
+        zoneSet2.add(zoneNN); zoneSet2.add(zoneSS); zoneSet2.add(zoneEE);
+        Area<Zone.Forest> forestAreaTest2 = new Area<>(zoneSet2,playerColorList,2);
+        Area<Zone.Forest> forestAreaTest = new Area<>(zoneSet1,playerColorList,2);
+        areaSet.add(forestAreaTest2); areaSet.add(forestAreaTest);
+        ZonePartition <Zone.Forest> test = new ZonePartition<>(areaSet);
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> test.areaContaining(zoneE));
+    }
+
+    @Test
+    void addSingletonTestNormal(){
+        Set<Zone.Forest> zoneSet1 = new HashSet<>();
+        Set<Zone.Forest> zoneSet2 = new HashSet<>();
+        Set<Zone.Forest> zoneSingleSet = new HashSet<>();
+        Set<Area<Zone.Forest>> areaSet = new HashSet<>();
+        Set<Area<Zone.Forest>> areaSet2 = new HashSet<>();
+        List<PlayerColor> playerColorList = new ArrayList<>();
+
+        Zone.Forest zoneN = new Zone.Forest(10, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneE = new Zone.Forest(11, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneS = new Zone.Forest(12, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneNN = new Zone.Forest(13, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneEE = new Zone.Forest(14, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneSS = new Zone.Forest(15, Zone.Forest.Kind.PLAIN);
+        Zone.Forest zoneSingle = new Zone.Forest(16, Zone.Forest.Kind.PLAIN);
+        //On ajoute des Zones dans des Sets
+        zoneSingleSet.add(zoneSingle);
+        zoneSet1.add(zoneN);zoneSet1.add(zoneE);zoneSet1.add(zoneS);
+        zoneSet2.add(zoneNN); zoneSet2.add(zoneSS); zoneSet2.add(zoneEE);
+        // On crée des Area
+        Area<Zone.Forest> forestAreaTest2 = new Area<>(zoneSet2,playerColorList,2);
+        Area<Zone.Forest> forestAreaTest = new Area<>(zoneSet1,playerColorList,2);
+        Area<Zone.Forest> forestAreaSingle = new Area<>(zoneSingleSet,playerColorList,4);
+        //On ajoute nos Area dans des sets d'areas
+        areaSet.add(forestAreaTest2); areaSet.add(forestAreaTest);
+        areaSet2.add(forestAreaTest2); areaSet2.add(forestAreaTest); areaSet2.add(forestAreaSingle);
+        //Rien
+        Set<Zone.Forest> zoneSetModifiable = new HashSet<>(zoneSet1);
+        Area<Zone.Forest> areaSetModfiable = new Area<>(zoneSetModifiable,playerColorList,4);
+        //On crée les builders
+        ZonePartition <Zone.Forest> testbuilder = new ZonePartition<>(areaSet);
+        ZonePartition <Zone.Forest> testSansBuilder = new ZonePartition<>(areaSet2);
+        ZonePartition.Builder<Zone.Forest> testBuilder = new ZonePartition.Builder<>(testbuilder);
+        //on utilise addSingelton
+
+        testBuilder.addSingleton(zoneSingle,4);
+        //c'est censé marcher damnn
+        assertEquals(testBuilder.build(),testSansBuilder);
+
+
+    }
+
+
+
+
+}
