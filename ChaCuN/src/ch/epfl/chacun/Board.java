2a759e9879330eeec76b0e5b4a25613376e1f049
touunyy <tony.andriamampianina@epfl.ch>
2024-03-20 20:31:55 +0100

    board done

--- ChaCuN/src/ch/epfl/chacun/Board.java
+++ ChaCuN/src/ch/epfl/chacun/Board.java
@@ -1,5 +1,7 @@
 package ch.epfl.chacun;
 
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Copy;
+
 import java.util.*;
 
 public final class Board {
@@ -18,7 +20,7 @@ public final class Board {
     this.cancelledAnimals = cancelledAnimals;
     }
     public static final int REACH = 12;
-    public static final Board EMPTY = new Board(new PlacedTile[625], new int[96], ZonePartitions.EMPTY, new HashSet<>());
+    public static final Board EMPTY = new Board(new PlacedTile[625], new int[0], ZonePartitions.EMPTY, new HashSet<>());
 
     public Set<Animal> cancelledAnimals () {return Set.copyOf(cancelledAnimals);}
     public Set<Occupant> occupants() {
@@ -39,6 +41,18 @@ public final class Board {
     }
     public Set <Area <Zone.Water>> riverSystemAreas() {
         return zonePartitions.riverSystems().areas();
+    }
+
+    public PlacedTile tileAt(Pos pos){
+        int indexTile  = (pos.y() - 12) * 25 + (pos.x() - 12);
+        return placedTiles[indexTile];
+    }
+
+    public PlacedTile tileWithId(int tileId){
+        for(int tileIndex : indexes){
+            if(placedTiles[tileIndex].id() == tileId) return placedTiles[tileIndex];
+        }
+        throw new IllegalArgumentException("la tuile n'appartient pas au plateau");
     }
     private boolean hasTile(Pos pos) {
         for (PlacedTile placedTile : placedTiles) {
@@ -92,22 +106,79 @@ public final class Board {
         TileSide sideS = tile.side(Direction.S);
         TileSide sideW = tile.side(Direction.W);
         if (insertionPositions().contains(tile.pos())) {
-            boolean canN = tileAt(tile.pos().neighbor(Direction.N) == null || oppositeSideSameType(tile, Direction.N));
-            boolean canE = tileAt(tile.pos().neighbor(Direction.E) == null || oppositeSideSameType(tile, Direction.E));
-            boolean canS = tileAt(tile.pos().neighbor(Direction.S) == null || oppositeSideSameType(tile, Direction.S));
-            boolean canW = tileAt(tile.pos().neighbor(Direction.W) == null || oppositeSideSameType(tile, Direction.W));
+            boolean canN = tileAt(tile.pos().neighbor(Direction.N)) == null || oppositeSideSameType(tile, Direction.N);
+            boolean canE = tileAt(tile.pos().neighbor(Direction.E)) == null || oppositeSideSameType(tile, Direction.E);
+            boolean canS = tileAt(tile.pos().neighbor(Direction.S))== null || oppositeSideSameType(tile, Direction.S);
+            boolean canW = tileAt(tile.pos().neighbor(Direction.W)) == null || oppositeSideSameType(tile, Direction.W);
             return (canN && canE && canS && canW);
         } return false;
     }
     public boolean couldPlaceTile (Tile tile ) {
         for (Pos p : insertionPositions()) {
             for (Rotation rotation : Rotation.values()) {
-                if (canAddTile(new PlacedTile(tile, null, rotation, p, null))
+                if (canAddTile(new PlacedTile(tile, null, rotation, p, null)))
                     return true;
             }
         } return false;
     }
+    private int posToNumber(Pos pos) {
+        int xNorm = pos.x() + 12;
+        int yNorm = pos.y() + 12;
+        return (yNorm*25 + xNorm+1);
+    }
+    private PlacedTile neighborTile (Pos pos, Direction direction) {
+        return switch (direction) {
+            case N -> tileAt(pos.neighbor(Direction.N));
+            case S -> tileAt(pos.neighbor(Direction.S));
+            case E -> tileAt(pos.neighbor(Direction.E));
+            case W -> tileAt(pos.neighbor(Direction.W));
+        };
+    }
+
     public Board withNewTile(PlacedTile tile) {
+        //si le board est vide ou la tile peut être posée
+        if (indexes.length == 0 || canAddTile(tile)){
+            //maj des arrays de placedtiles et indexes
+            PlacedTile [] newPT = Arrays.copyOf(this.placedTiles, this.placedTiles.length);
+            newPT [posToNumber(tile.pos())] = tile;
+            int [] newInd = Arrays.copyOf(this.indexes, this.indexes.length + 1);
+            newInd [newInd.length-1] = posToNumber(tile.pos());
+
+            //maj de zonepartitions
+            ZonePartitions.Builder zBuilder = new ZonePartitions.Builder(zonePartitions);
+            for (Direction direction : Direction.ALL) {
+                if (neighborTile(tile.pos(), direction) != null)
+                    zBuilder.connectSides(tile.side(direction), neighborTile(tile.pos(), direction).side(direction.rotated(Rotation.HALF_TURN)));
+            }
+            return new Board(newPT, newInd, zBuilder.build(), cancelledAnimals());
+        } throw new IllegalArgumentException("Board not empty AND tile cannot be added to board");
+    }
+
+    public Board withoutOccupant (Occupant occupant) {
+        PlacedTile[] PT = placedTiles.clone();
+        int [] ind = indexes.clone();
+        ZonePartitions.Builder builder = new ZonePartitions.Builder(zonePartitions);
+        PlacedTile tile = tileWithId(Zone.tileId(occupant.zoneId()));
+        Zone occupiedZone = tile.zoneWithId(occupant.zoneId());
+        tile.withNoOccupant();
+        builder.removePawn(tile.placer(), occupiedZone);
+
+        return (new Board(PT, ind, builder.build(), cancelledAnimals()));
 
     }
+    public Board withoutGatherersOrFishersIn(Set<Area<Zone.Forest>> forests, Set<Area<Zone.River>> rivers){
+        ZonePartitions.Builder zBuilder = new ZonePartitions.Builder(zonePartitions);
+        for (Area <Zone.Forest> forest : forests)
+            zBuilder.clearGatherers(forest);
+        for (Area <Zone.River> river : rivers)
+            zBuilder.clearFishers(river);
+        return (new Board (placedTiles.clone(), indexes.clone(), zBuilder.build(), cancelledAnimals()));
+    }
+    public Board withMoreCancelledAnimals (Set<Animal> newlyCancelledAnimals) {
+        Set <Animal> newCancelled = new HashSet<>(cancelledAnimals());
+        newCancelled.addAll(newlyCancelledAnimals);
+        return new Board(placedTiles.clone(), indexes.clone(), new ZonePartitions.Builder(zonePartitions).build(), Collections.unmodifiableSet(newCancelled));
+    }
+
 }
+
