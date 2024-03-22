ec6621252ba60884562e250a2462745e7520b6c5
hassenBC <hassen.benchaabane@epfl.ch>
2024-03-22 14:11:14 +0100

    j'ai corrigé les erreurs de Area

--- ChaCuN/src/ch/epfl/chacun/Board.java
+++ ChaCuN/src/ch/epfl/chacun/Board.java
@@ -30,12 +30,7 @@ public final class Board {
         }
         return occupants;
     }
-    public Area <Zone.Forest> forestArea(Zone.Forest forest) {
-        return zonePartitions.forests().areaContaining(forest);
-    }
-    public Area <Zone.Meadow> meadowArea(Zone.Meadow meadow) {
-        return zonePartitions.meadows().areaContaining(meadow);
-    }
+
     public Set <Area<Zone.Meadow>> meadowAreas() {
         return zonePartitions.meadows().areas();
     }
@@ -48,8 +43,12 @@ public final class Board {
         int yNorm = pos.y() + 12;
         return (yNorm*25 + xNorm);
     }
-    public PlacedTile tileAt(Pos pos){
-        return  (posToNumber(pos) >= 0 || posToNumber(pos) <= 624) ?  placedTiles[posToNumber(pos)] : null;
+    public PlacedTile tileAt(Pos pos) {
+        if (pos.x() < -12 || pos.x() > 12 || pos.y() < -12 || pos.y() > 12) {
+            return null;
+        }
+        int indexTile = (pos.y() + 12) * 25 + (pos.x() + 12);
+        return placedTiles[indexTile];
     }
 
     public PlacedTile tileWithId(int tileId){
@@ -74,13 +73,13 @@ public final class Board {
             for (int i = -12; i <= 12; i++) {
                 for (int j = -12; j <= 12; j++) {
                     Pos pos = new Pos(i, j);
-                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.N)) != null && pos.y() >= -11)
+                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.N)) != null)
                         posSet.add(pos);
-                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.E)) != null && pos.x() <= 11)
+                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.E)) != null)
                         posSet.add(pos);
-                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.S)) != null && pos.y() <= 11)
+                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.S)) != null)
                         posSet.add(pos);
-                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.W)) != null && pos.x() >= -11)
+                    if (tileAt(pos) == null && tileAt(pos.neighbor(Direction.W)) != null)
                         posSet.add(pos);
                 }
             }
@@ -183,6 +182,106 @@ public final class Board {
         newCancelled.addAll(newlyCancelledAnimals);
         return new Board(placedTiles.clone(), indexes.clone(), new ZonePartitions.Builder(zonePartitions).build(), Collections.unmodifiableSet(newCancelled));
     }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+    //**************************************************************************************************************************************
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+
+    public Area<Zone.Forest> forestArea(Zone.Forest forest) {
+        return zonePartitions.forests().areaContaining(forest);
+    }
+
+    public Area<Zone.Meadow> meadowArea(Zone.Meadow meadow) {
+        return zonePartitions.meadows().areaContaining(meadow);
+    }
+
+    public Area<Zone.River> riverArea(Zone.River riverZone) {
+        return zonePartitions.rivers().areaContaining(riverZone);
+    }
+
+    public Area<Zone.Water> riverSystemArea(Zone.Water water) {
+        return zonePartitions.riverSystems().areaContaining(water);
+    }
+
+    // fait une deuxieme boucle avec les lambdas pour les occupants
+    public Area<Zone.Meadow> adjacentMeadow(Pos pos, Zone.Meadow meadowZone) {
+
+        Set<Zone.Meadow> adjacentMeadows = new HashSet<>();
+        List<PlayerColor> playerColorList = meadowArea(meadowZone).occupants();
+        Set<Zone.Meadow> meadowAdjZones = meadowArea(meadowZone).zones();
+
+        for (int index : validIndexes(pos.x(), pos.y()) ) {
+            for (Zone.Meadow zone : placedTiles[index].meadowZones()) {
+                if (meadowAdjZones.contains(zone)) {
+                    adjacentMeadows.add(zone);
+                }
+            }
+        }
+        return new Area<>(adjacentMeadows, playerColorList, 0);
+    }
+
+    public int occupantCount(PlayerColor player, Occupant.Kind occupantKind) {
+        int playerCount = 0;
+        for (int tileIndex : indexes) {
+            if(placedTiles[tileIndex].occupant() !=null){
+                if ( placedTiles[tileIndex].occupant().kind().equals(occupantKind) && placedTiles[tileIndex].placer().equals(player)) {
+                    playerCount++;
+                }
+            }
+        }
+        return playerCount;
+    }
+
+    //qui retourne l'ensemble de toutes les aires forêts qui ont été
+    // fermées suite à la pose de la dernière tuile, ou un ensemble vide si le plateau est vide,
+    public Set<Area<Zone.Forest>> forestsClosedByLastTile() {
+        Set<Area<Zone.Forest>> forestAreas = new HashSet<>();
+        if (indexes.length == 0) {
+            return new HashSet<>();
+        }
+        for (Zone.Forest zoneForest : placedTiles[indexes.length - 1].forestZones()) {
+            forestAreas.add(forestArea(zoneForest));
+        }
+        forestAreas.removeIf(ForestArea -> ForestArea.openConnections() != 0);
+
+        return forestAreas;
+    }
+
+    public Set<Area<Zone.River>> riversClosedByLastTile() {
+        Set<Area<Zone.River>> riverAreas = new HashSet<>();
+        if (indexes.length == 0) return new HashSet<>();
+        for (Zone.River river : placedTiles[indexes.length - 1].riverZones()) {
+            riverAreas.add(riverArea(river));
+        }
+        riverAreas.removeIf(RiverArea -> RiverArea.openConnections() != 0);
+        return riverAreas;
+    }
+
+    public Board withOccupant(Occupant occupant) {
+        int occupantTileId = Zone.tileId(occupant.zoneId());
+
+        PlacedTile[] updatedPlacedTiles = placedTiles.clone();
+
+        for (int tileIndex : indexes) {
+            if (placedTiles[tileIndex].tile().id() == occupantTileId) {
+                if (placedTiles[tileIndex].occupant() != null)  throw new IllegalArgumentException("la zone est deja occupée");
+                updatedPlacedTiles[tileIndex].withOccupant(occupant);
+
+            }
+        }
+        //check immuabilité
+        return new Board(updatedPlacedTiles, indexes, zonePartitions, cancelledAnimals);
+    }
+
+//    public Board withMoreCancelledAnimals(Set<Animal> newlyCancelledAnimals) {
+//        Set<Animal> newCancelledAnimals = new HashSet<>(cancelledAnimals);
+//        newCancelledAnimals.addAll(newlyCancelledAnimals);
+//        newCancelledAnimals = Set.copyOf(newCancelledAnimals);
+//        return new Board(placedTiles, indexes, zonePartitions, newCancelledAnimals);
+//
+//    }
+
     @Override
     public boolean equals(Object objectBoard) {
         if (this == objectBoard) return true;
@@ -203,6 +302,33 @@ public final class Board {
         result = 31 * result + Arrays.hashCode(indexes);
         return result;
     }
+
+
+    private int getIndexFrom(int x ,int y ){
+        if (x < -12 || x > 12 || y < -12 || y > 12) {
+            return -1;
+        }
+        return (y + 12) * 25 + (x + 12);
+    }
+
+    private List<Integer> validIndexes(int x , int y){
+
+        List<Integer> validIndexesArray = new ArrayList<>();
+
+        int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
+        for (int i = 0; i < dx.length; i++) {
+            int adjX = x + dx[i];
+            int adjY = y + dy[i];
+            int index = getIndexFrom(adjX, adjY);
+
+            if (index != -1) {
+                validIndexesArray.add(index);
+            }
+        }
+        return validIndexesArray;
+    }
+
 
 }
 
